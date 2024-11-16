99692cfc89f15faa60996468868bb93124c5179b
gopikiran22001 <gopikiran22001@yahoo.com>
2024-11-16 11:09:22 +0530

    Create nqueens.py

---
+++ nqueens.py
@@ -0,0 +1,23 @@
+def place(k1,i1):
+    for j in range(k1):
+        if x[j]==i1 or abs(x[j]-i1)==abs(j-k1):
+            return False
+    return True
+def nqueens(k,n):
+    global x,co
+    for i in range(n+1):
+        if place(k,i):
+            x[k]=i
+            if k==n:
+                co+=1
+                print(x)
+            else:
+                nqueens(k+1,n)
+        
+x=[]
+co=0
+n=int(input())
+for i in range(n):
+    x.append(0)
+nqueens(0,n-1)
+print(co)
